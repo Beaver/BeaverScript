cfdb74c1d45cc54e05f2138a88e0a7e9e01ec96f
Theo <theo@scribd.com>
2017-11-01 21:16:49 -0700

    Fixed absolute path handling

--- BeaverScript/beaver.swift
+++ BeaverScript/beaver.swift
@@ -3,8 +3,11 @@
 import Commander
 import BeaverCodeGen
 
-guard let basePath = ProcessInfo.processInfo.environment["BASE_DIR"] else {
-    fatalError("Could not find env variable 'BASE_DIR'")
+func absolutePath(with path: String) -> String {
+    guard let basePath = ProcessInfo.processInfo.environment["BASE_DIR"] else {
+        fatalError("Could not find env variable 'BASE_DIR'")
+    }
+    return basePath + "/" + path.replacingOccurrences(of: basePath, with: "")
 }
 
 Group {
@@ -18,7 +21,7 @@ Group {
             .map { $0.trimmingCharacters(in: .whitespaces) }
             .filter { !$0.isEmpty }
         
-        let fileHandler = FileHandler(basePath: "\(basePath)/\(path)")
+        let fileHandler = FileHandler(basePath: absolutePath(with: path))
         let generator = ProjectGenetator(name: name,
                                          moduleNames: moduleNames)
 
@@ -29,7 +32,7 @@ Group {
         Argument<String>("project_name", description: "Your project name"),
         Argument<String>("module_name", description: "Your module name")
     ) { path, projectName, moduleName in
-        let fileHandler = FileHandler(basePath: "\(basePath)/\(path)")
+        let fileHandler = FileHandler(basePath: absolutePath(with: path))
         let generator = ProjectGenetator(name: projectName,
                                          moduleNames: [])
         _ = generator.byInserting(module: moduleName, in: fileHandler)
@@ -41,7 +44,7 @@ Group {
        Argument<String>("action_name", description: "Your action name"),
        Argument<String>("action_type", description: "Your action type: [ui|routing]")
     ) { path, projectName, moduleName, actionName, actionType in
-        let fileHandler = FileHandler(basePath: "\(basePath)/\(path)")
+        let fileHandler = FileHandler(basePath: absolutePath(with: path))
         let generator = ProjectGenetator(name: projectName,
                                          moduleNames: [])
         _ = generator.byInserting(action: actionType == "ui" ? .ui(EnumCase(name: actionName)) : .routing(EnumCase(name: actionName)),
